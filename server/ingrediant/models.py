bb56e52184bc3deeb73836c9f99ac854e9398194
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-13 12:01:12 -0500

    Add: Ingrediant and IngrediantInfo models.

--- server/ingrediant/models.py
+++ server/ingrediant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Ingrediant(models.Model):
+    name = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    dateAdded = models.DateTimeField(auto_now_add=True, null=False)
+
+class IngrediantInfo(models.Model):
+    name = models.OneToOneField(Ingrediant.name, on_delete=models.CASCADE, null=False)
+    description = models.CharField(max_length=500, null=True)
+    cost = models.CharField(max_length=150, null=True)
+    comments = models.CharField(max_length=400, null=False)
