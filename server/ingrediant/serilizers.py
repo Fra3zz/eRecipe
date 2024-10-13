99a6c65b62201f009fc8472546f878dcb33e99e0
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-13 16:39:32 -0500

    Add: IngredientInfo serilizer. Add: IngredientInfo GET/POST API endpoint.

--- server/ingrediant/serilizers.py
+++ server/ingrediant/serilizers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Ingredient, IngredientInfo
 
+
+#Ingredient Model Serilizer
 class IngredientSerilizer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ["name", "id"]
+        
+#IngredientInfos Model Serilizer
+class IngredientInfoSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = IngredientInfo
+        fields = ["name", "description", "cost", "comments", "id"]
--- server/ingrediant/urls.py
+++ server/ingrediant/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import getAdd, removeUpdate
+from .views import getAdd, removeUpdate, ingredientInfoGetAddView
 
 urlpatterns = [
-    path("", getAdd),
-    path("update/<pk>", removeUpdate)
+    #Ingredients Model API Endpoints
+    path("ingredient/", getAdd),
+    path("ingredient/update/<pk>", removeUpdate),
+    
+    #INgredientsInfo Model API Endpoints
+    path("info/", ingredientInfoGetAddView)
     
 ]
