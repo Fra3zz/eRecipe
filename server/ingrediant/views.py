b55abc0342d7b18c1a62a88f6448b6f8745c2614
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-13 13:27:36 -0500

    Add: addIngrediant view for POST api endpoint.

--- server/ingrediant/views.py
+++ server/ingrediant/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .serilizers import IngrediantSerilizer
+from .serilizers import IngredientSerilizer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Ingrediant
+from .models import Ingredient
 from rest_framework import status
+from rest_framework.parsers import JSONParser
+import json
 
 
 
@@ -12,7 +14,16 @@ from rest_framework import status
 @api_view(['GET'])
 def getIngrediant(request):
     if request.method == 'GET':
-        ingredintData = Ingrediant.objects.all()
-        serilizedIngrediants = IngrediantSerilizer(ingredintData).data
+        ingredintData = Ingredient.objects.all()
+        serilizedIngrediants = IngredientSerilizer(ingredintData).data
         return Response(serilizedIngrediants)
     return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(["POST"])
+def addIngrediant(request):
+    serilizedData = IngredientSerilizer(data=request.data)
+    if serilizedData.is_valid():
+        serilizedData.save()
+        return Response(serilizedData.data, status=status.HTTP_201_CREATED)
+    return Response(serilizedData.errors, status=status.HTTP_400_BAD_REQUEST)
+        
