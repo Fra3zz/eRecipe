af802fee96abb6a72470f0a84351bc53d3e5f248
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-13 12:34:19 -0500

    Add: getIngrediant view Add: Registered models in admin

--- server/ingrediant/admin.py
+++ server/ingrediant/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ingrediant, IngrediantInfo
 
-# Register your models here.
+admin.site.register(Ingrediant)
+admin.site.register(IngrediantInfo)
--- server/ingrediant/views.py
+++ server/ingrediant/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .serilizers import IngrediantSerilizer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Ingrediant
+from rest_framework import status
+
+
 
 # Create your views here.
+
+@api_view(['GET'])
+def getIngrediant(request):
+    if request.method == 'GET':
+        ingredintData = Ingrediant.objects.all()
+        serilizedIngrediants = IngrediantSerilizer(ingredintData).data
+        return Response(serilizedIngrediants)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
