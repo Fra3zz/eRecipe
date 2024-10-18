e68c4481aab8d269c8791ef8b29f3d4054141b5f
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-18 16:27:09 -0500

    Update: Cleaned up api endpoints.

--- server/recipe/urls.py
+++ server/recipe/urls.py
@@ -3,11 +3,11 @@ from .views import getAddRecipeView, updateRecipeView, getAddRecipeIngredientsVi
 
 urlpatterns = [
     # Recipe Model API Endpoints
-    path("recipes/", getAddRecipeView),
-    path("recipes/<str:recipe_name>/update", updateRecipeView),
+    path("recipes", getAddRecipeView),
+    path("recipes/<str:recipe_name>", updateRecipeView),
     
     # Recipe Ingredient Model API Endpoints
-    path("ingredients/", getAddRecipeIngredientsView), #Gets all ingredients for all recipes.
-    path("ingredients/<str:recipe_name>/<str:ingredient_name>", updateRecipeIngredientView), #Allows for specific selection of ingredient in recipe
-    path("recipe/<str:recipe_name>/ingredients/", getRecipeIngredientsByRecipeNameView) #Gets the ingredients for a specific recipe.
+    path("ingredients", getAddRecipeIngredientsView), #Gets all ingredients for all recipes.
+    path("ingredients/<str:recipe_name>", getRecipeIngredientsByRecipeNameView), #Gets the ingredients for a specific recipe.
+    path("ingredients/<str:recipe_name>/<str:ingredient_name>", updateRecipeIngredientView), #Update/Delete ingrediant of a specific recipe.
 ]
