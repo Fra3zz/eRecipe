c5d8aebe53c65742f5582f6b44e2db559c4e9109
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-18 16:32:18 -0500

    Add: URL names Add: API Endpoint Test

--- server/recipe/urls.py
+++ server/recipe/urls.py
@@ -3,11 +3,11 @@ from .views import getAddRecipeView, updateRecipeView, getAddRecipeIngredientsVi
 
 urlpatterns = [
     # Recipe Model API Endpoints
-    path("recipes", getAddRecipeView),
-    path("recipes/<str:recipe_name>", updateRecipeView),
+    path("recipes", getAddRecipeView,name="get_add_recipe"),
+    path("recipes/<str:recipe_name>", updateRecipeView, name="update_recipe"),
     
     # Recipe Ingredient Model API Endpoints
-    path("ingredients", getAddRecipeIngredientsView), #Gets all ingredients for all recipes.
-    path("ingredients/<str:recipe_name>", getRecipeIngredientsByRecipeNameView), #Gets the ingredients for a specific recipe.
-    path("ingredients/<str:recipe_name>/<str:ingredient_name>", updateRecipeIngredientView), #Update/Delete ingrediant of a specific recipe.
+    path("ingredients", getAddRecipeIngredientsView, name="get_add_recipe_ingredients"), #Gets all ingredients for all recipes.
+    path("ingredients/<str:recipe_name>", getRecipeIngredientsByRecipeNameView, name="get_recipe_ingredients"), #Gets the ingredients for a specific recipe.
+    path("ingredients/<str:recipe_name>/<str:ingredient_name>", updateRecipeIngredientView,  name="update_recipe_ingredient"), #Update/Delete ingrediant of a specific recipe.
 ]
