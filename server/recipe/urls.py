0117332c40a3c2df4fc90e4bdf02c05ab70208c2
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-19 12:14:14 -0500

    Update: Finished path for getting all ingredients.

--- server/recipe/urls.py
+++ server/recipe/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("<str:recipe_name>", updateRecipeView, name="update_recipe"),
     
     # Recipe Ingredient Model API Endpoints
-    path("ingredients", getAddRecipeIngredientsView, name="get_add_recipe_ingredients"), #Gets all ingredients for all recipes.
+    path("ingredients/", getAddRecipeIngredientsView, name="get_add_recipe_ingredients"), #Gets all ingredients for all recipes.
     path("ingredients/<str:recipe_name>", getRecipeIngredientsByRecipeNameView, name="get_recipe_ingredients"), #Gets the ingredients for a specific recipe.
     path("ingredients/<str:recipe_name>/<str:ingredient_name>", updateRecipeIngredientView,  name="update_recipe_ingredient"), #Update/Delete ingrediant of a specific recipe.
 ]
