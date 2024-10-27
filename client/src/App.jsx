18113c534a2fe1fa3c48d4df3be55831783ef506
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-10-27 14:50:33 -0500

    Add: NavBar with links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 
 import ROUTES from "./components/routes"
+import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <div>
+      <NavBar/>
       <ROUTES />
     </div>
   )
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <br />
+
+            <a href="/ingredients/">View/Add Ingredients</a>
+            <br />
+
+
+            <a href="/ingredients/update/">Update Ingredient</a>
+            <br />
+
+            <a href="/add/recipe/">Add Recipe</a>
+            <br />
+
+            <a href="/add/recipe/ingredient/">Add Ingredient to Recipe</a>
+
+        </div>
+    )
+}
+
+export default NavBar
