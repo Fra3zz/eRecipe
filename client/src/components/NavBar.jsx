998a6153f68934727df544478ff614b9ba115b69
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-11-03 15:10:23 -0600

    Update: Enclosed app and routes in propper BrowserRouter tags Add: Bootstrap dropdown menu.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,25 +1,31 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const NavBar = () => {
     return (
-        <div>
-            <a href="/">Home</a>
-            <br />
+        <nav className="navbar">
+            <ul className="nav">
+                <li className="nav-item dropdown">
+                    <a 
+                        className="nav-link dropdown-toggle" 
+                        data-bs-toggle="dropdown" 
+                        href="#" 
+                        role="button" 
+                        aria-expanded="false"
+                    >
+                        Menu
+                    </a>
+                    <ul className="dropdown-menu">
+                        <li><a className="dropdown-item" href="/">Home</a></li>
+                        <li><a className="dropdown-item" href="/ingredients/">View/Add Ingredients</a></li>
+                        <li><a className="dropdown-item" href="/ingredients/update/">Update Ingredient</a></li>
+                        <li><a className="dropdown-item" href="/add/recipe/">Add Recipe</a></li>
+                        <li><a className="dropdown-item" href="/add/recipe/ingredient/">Add Ingredient to Recipe</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
-            <a href="/ingredients/">View/Add Ingredients</a>
-            <br />
-
-
-            <a href="/ingredients/update/">Update Ingredient</a>
-            <br />
-
-            <a href="/add/recipe/">Add Recipe</a>
-            <br />
-
-            <a href="/add/recipe/ingredient/">Add Ingredient to Recipe</a>
-
-        </div>
-    )
-}
-
-export default NavBar
+export default NavBar;
--- client/src/components/routes.jsx
+++ client/src/components/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./Home/home";
 import GetIngredients from "./ingredients/IngredientsRequest";
 import UpdateIngredients from "./ingredients/updateIngredients";
@@ -9,29 +9,23 @@ import GetRecipeIngredients from "./recipe/getRecipeINgredients";
 
 export default function ROUTES() {
     return (
-        <div>
-            <BrowserRouter>
-                <main>
-                    <Routes> 
+        <main>
+            <Routes>
+                {/* Home Page */}
+                <Route path="/home/" element={<Home />} />
 
-                        {/* Home Page */}
-                        <Route path="/home/" element={<Home />} />
+                {/* Ingredient app */}
+                <Route path="/ingredients/" element={<GetIngredients />} />
+                <Route path="/ingredients/update/" element={<UpdateIngredients />} />
 
-                        {/* Ingredient app */}
-                        <Route path="/ingredients/" element={<GetIngredients />} /> {/* Ingredients Page: Add and View */}
-                        <Route path="/ingredients/update/" element={<UpdateIngredients />} /> {/* Update ingredients page */}
+                {/* Recipe app */}
+                <Route path="/" element={<GetRecipes />} />
+                <Route path="/add/recipe/" element={<AddRecipe />} />
+                <Route path="/add/recipe/ingredient/" element={<AddIngredientToRecipe />} />
 
-                        {/* Recipe app */}
-                        <Route path="/" element={<GetRecipes />} />
-                        <Route path="/add/recipe/" element={<AddRecipe />} />
-                        <Route path="/add/recipe/ingredient/" element={<AddIngredientToRecipe />} />
-
-                        {/* Recipe Ingredients Route */}
-                        <Route path="/recipe/ingredients/:recipeName" element={<GetRecipeIngredients />} />
-
-                    </Routes>
-                </main>
-            </BrowserRouter>
-        </div>
+                {/* Recipe Ingredients Route */}
+                <Route path="/recipe/ingredients/:recipeName" element={<GetRecipeIngredients />} />
+            </Routes>
+        </main>
     );
 }
