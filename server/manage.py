a9fef91467721ef5cca7c6a433ffdf6b8e3d2732
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-11-07 20:19:40 -0600

    Remove: Open domain range.

--- server/manage.py
+++ server/manage.py
@@ -2,10 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.utils.regex_helper import _lazy_re_compile
-import django.http.request
 
-django.http.request.host_validation_re = _lazy_re_compile(r"[a-zA-z0-9.:]*")
+
 
 
 
